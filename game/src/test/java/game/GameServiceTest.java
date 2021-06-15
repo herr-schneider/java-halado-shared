e5e83c3f37668582a14df16ae5791045461f17fb
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-06-15 18:01:40 +0200

    third commit

--- game/src/test/java/game/GameServiceTest.java
+++ game/src/test/java/game/GameServiceTest.java
@@ -2,8 +2,14 @@ package game;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class GameServiceTest {
 
@@ -15,7 +21,7 @@ public class GameServiceTest {
     }
 
 
-    @Disabled
+    @Test
     void testGetGameWithMostGoalDifference() {
         Game expected = gameService.getGameWithMostGoalDifference();
 
