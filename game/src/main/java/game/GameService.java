445d3134b936cb757ca66339fcd76e27bdd201c5
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-06-15 16:51:27 +0200

    second commit

--- game/src/main/java/game/GameService.java
+++ game/src/main/java/game/GameService.java
@@ -1,6 +1,28 @@
 package game;
 
-public class GameService {
+import java.util.List;
 
-    public String getCountryWithMostGoal
+public class GameService {
+    private GameRepository gameRepository;
+
+    public String getCountryWithMostGoal() {
+        String result = "Nincs ilyen!";
+        int maxGoal = 0;
+        List<Game> games = gameRepository.getGames();
+        for (Game game : games) {
+            if (Integer.parseInt(game.getFirstCountryScore()) > Integer.parseInt(game.getSecondCountryScore())) {
+                if (Integer.parseInt(game.getFirstCountryScore()) > maxGoal) {
+                    result = game.getFirstCountry();
+                    maxGoal = Integer.parseInt(game.getFirstCountryScore());
+                } else {
+                }
+            } else {
+                if (Integer.parseInt(game.getSecondCountryScore()) > maxGoal) {
+                    result = game.getSecondCountry();
+                    maxGoal = Integer.parseInt(game.getSecondCountryScore());
+                }
+            }
+        }
+        return result;
+    }
 }
