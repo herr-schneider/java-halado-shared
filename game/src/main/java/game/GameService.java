a45719418d14734c9fb51baa1f608f415bb63eb2
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-06-15 17:50:02 +0200

    third commit

--- game/src/main/java/game/GameService.java
+++ game/src/main/java/game/GameService.java
@@ -12,43 +12,43 @@ public class GameService {
     }
 
     public String getCountryWithMostGoals() {
-        Map<String, Integer> result = new HashMap<>();
-        int actual;
-        int maxBattle = 0;
-        String country = "Nincs";
-        for (Game game : gameRepository.getGames()) {
-            if (!result.containsKey(game.getFirstCountry())) {
-                result.put(game.getFirstCountry(), Integer.parseInt(game.getFirstCountryScore()));
-            } else {
-                actual = result.get(game.getFirstCountry());
-                result.replace(game.getFirstCountry(), actual + Integer.parseInt(game.getFirstCountryScore()));
-                if (actual > maxBattle) {
-                    maxBattle = actual + actual + Integer.parseInt(game.getFirstCountryScore());
-                    country = game.getFirstCountry();
+            Map<String, Integer> result = new HashMap<>();
+            int actual;
+            int maxBattle = 0;
+            String country = "Nincs";
+            for (Game game : gameRepository.getGames()) {
+                if (!result.containsKey(game.getFirstCountry())) {
+                    result.put(game.getFirstCountry(), Integer.parseInt(game.getFirstCountryScore()));
+                } else {
+                    actual = result.get(game.getFirstCountry());
+                    result.replace(game.getFirstCountry(), actual + Integer.parseInt(game.getFirstCountryScore()));
+                    if (actual > maxBattle) {
+                        maxBattle = actual + actual + Integer.parseInt(game.getFirstCountryScore());
+                        country = game.getFirstCountry();
+                    }
+                }
+                if (!result.containsKey(game.getSecondCountry())) {
+                    result.put(game.getSecondCountry(), Integer.parseInt(game.getSecondCountryScore()));
+                } else {
+                    actual = result.get(game.getSecondCountry());
+                    result.replace(game.getSecondCountry(), actual + Integer.parseInt(game.getSecondCountryScore()));
+                    if (actual > maxBattle) {
+                        maxBattle = actual + actual + Integer.parseInt(game.getSecondCountryScore());
+                        country = game.getSecondCountry();
+                    }
                 }
             }
-            if (!result.containsKey(game.getSecondCountry())) {
-                result.put(game.getSecondCountry(), Integer.parseInt(game.getSecondCountryScore()));
-            } else {
-                actual = result.get(game.getSecondCountry());
-                result.replace(game.getSecondCountry(), actual + Integer.parseInt(game.getSecondCountryScore()));
-                if (actual > maxBattle) {
-                    maxBattle = actual + actual + Integer.parseInt(game.getSecondCountryScore());
-                    country = game.getSecondCountry();
-                }
-            }
-        }
-        return country;
+            return country;
     }
-}
+
 
     public int getGoals(String country) {
         int goals = 0;
         for (Game g : gameRepository.getGames()) {
             if (g.getFirstCountry().equals(country)) {
-                goals += g.getFirstCountryScore();
+                goals += Integer.parseInt(g.getFirstCountryScore());
             } else if (g.getSecondCountry().equals(country)) {
-                goals += g.getSecondCountryScore();
+                goals += Integer.parseInt(g.getSecondCountryScore());
             }
         }
         return goals;
